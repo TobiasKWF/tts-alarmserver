7794756016fe8c97afc76608b450aa1cfe294f8e
TobiasK <info@tobiaskoenig.de>
2026-07-01 09:52:59 +0200

    Create request-id.js

---
+++ src/middleware/request-id.js
@@ -0,0 +1,11 @@
+const { randomUUID } = require("crypto");
+
+module.exports = (req, res, next) => {
+
+    req.requestId = randomUUID();
+
+    res.setHeader("X-Request-Id", req.requestId);
+
+    next();
+
+};
